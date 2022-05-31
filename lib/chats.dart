6238ce9cde859aa7378f37478a4b6706cbbb8ea4
FlutLab.io <>
2022-05-31 08:04:26 +0000

    Autosave

--- lib/chats.dart
+++ lib/chats.dart
@@ -22,6 +22,27 @@ class Chats extends StatelessWidget {
                 backgroundImage: NetworkImage('https://assets.skor.id/crop/0x0:0x0/x/photo/2021/07/29/4006111797.jpg'),
                 radius: 35,
               ),
+              title: Text(
+                "Jess No Limit",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Row(
+                children: [
+                  Icon(Icons.done_all, color: Colors.lightBlue),
+                  Text(
+                    "Mabar",
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+              trailing: Text("07:00",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  )),
             ),
           ],
         ),
